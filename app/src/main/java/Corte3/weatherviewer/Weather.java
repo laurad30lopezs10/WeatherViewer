2fe610bdb9f303a594f8c394320c29af5d95076f
Laura <laura.daniela.lopez.3010@gmail.com>
2024-05-18 19:42:14 -0500

    se hizo la interfaz asi como list_item.xml se creo la clase Weather.java

---
+++ app/src/main/java/Corte3/weatherviewer/Weather.java
@@ -0,0 +1,62 @@
+package Corte3.weatherviewer;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Weather {
+    public String dayOfWeek;
+    public final String minTemp;
+    public final String maxTemp;
+    public final String humidity;
+    public final String description;
+    public final String iconURL;
+
+    public Weather(String dayOfWeek, String minTemp, String maxTemp, String humidity,
+                   String description, String iconURL) {
+        this.dayOfWeek = dayOfWeek;
+        this.minTemp = formatTemperature(minTemp);
+        this.maxTemp = formatTemperature(maxTemp);
+        this.humidity = formatPercentage(humidity);
+        this.description = description;
+        this.iconURL = iconURL;
+
+        // Convertir la marca de tiempo a nombre de día
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE", Locale.getDefault());
+        try {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(dayOfWeek));
+            this.dayOfWeek = sdf.format(cal.getTime());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String formatTemperature(String temp) {
+        NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
+        nf.setMaximumFractionDigits(0);
+        return nf.format(Double.parseDouble(temp)) + "\u00B0F";
+    }
+
+    private String formatPercentage(String percent) {
+        double value = Double.parseDouble(percent) / 100.0;
+        NumberFormat nf = NumberFormat.getPercentInstance(Locale.US);
+        return nf.format(value);
+    }
+
+    private static String convertTimeStampToDay(long timeStamp) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timeStamp * 1000);
+        TimeZone tz = TimeZone.getDefault();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        sdf.setTimeZone(tz);
+
+        String dayName = sdf.format(calendar.getTime());
+        return dayName;
+    }
+
+}
+
