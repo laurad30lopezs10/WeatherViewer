9fa9f5be24aaea050b09b9cdea28fe9fae4e2955
Laura <laura.daniela.lopez.3010@gmail.com>
2024-05-19 16:24:50 -0500

    se trabajo en la MainActivity.java2

--- app/src/main/java/Corte3/weatherviewer/MainActivity.java
+++ app/src/main/java/Corte3/weatherviewer/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.ListView;
 import android.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,6 +28,7 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -70,8 +72,78 @@ public class MainActivity extends AppCompatActivity {
                 URL url = createURL (locationEditText.getText().toString());
 
                 if (url != null){
-                    dismissKeyboardShortcutsHelper();
+                    dismissKeyboard(locationEditText);
+                    GetWeatherTask getLocalWeatherTask= new GetWeatherTask();
+                    getLocalWeatherTask.execute(url);
                 }
+                else {
+                    Snackbar.make(findViewById(R.id.coordinatorLayaout),
+                            R.string.url_invalida, Snackbar.LENGTH_LONG);
+
+                }
+            }
+            private void dismissKeyboard(View view){
+                InputMethodManager imm = (InputMethodManager) getSystemService(
+                        Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+            }
+
+            private URL createURL(String city){
+                String apiKey = getString(R.string.api_key);
+                String baseUrl = getString(R.string.web_service_url);
+
+                try{
+                    String urlSring = baseUrl + URLEncoder.encode(city, "UFT-8")+
+                            "&UNITS=IMPERIAL&CNT=16&APPID="+ apiKey;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            private class GetWeatherTask
+                    extends AsyncTask<URL, Void, JSONObject>{
+
+                public JSONObject doInBackground(URL... params){
+                    HttpURLConnection connection= null;
+
+                    try {
+                        connection = (HttpURLConnection) params[0].openConnection();
+                        int response = connection.getResponseCode();
+
+                        if (response == HttpURLConnection.HTTP_OK) {
+                            StringBuilder builder = new StringBuilder();
+
+                            try (BufferedReader reader = new BufferedReader(
+                                    new InputStreamReader(connection.getInputStream()))) {
+
+                                String line;
+
+                                while (line = reader.readLine() != null) {
+                                    builder.append(line);
+                                }
+                            } catch (IOException e) {
+                                Snackbar.make(findViewById(R.id.coordinatorLayaout),
+                                        R.string.Error_lecura, Snackbar.LENGTH_LONG).show();
+                                e.printStackTrace();
+                            }
+                            return new JSONObject(builder.toString());
+                        }
+                        else {
+                            Snackbar.make(findViewById(R.id.coordinatorLayaout),
+                                    R.string.Errro_conection,Snackbar.LENGTH_LONG).show();
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    } catch (Exception e) {
+                        Snackbar.make(findViewById(R.id.coordinatorLayaout),
+                                R.string.Errro_conection, Snackbar.LENGTH_LONG).show();
+                    } finally {
+                        connection.disconnect();
+                    }
+                    return null;
+                }
+
             }
         });
 
